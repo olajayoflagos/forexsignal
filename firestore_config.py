42c85daa195aa8acbfff3ea4adf978746e62c1f5
olajayoflagos <jaystar1000.ja@gmail.com>
2025-06-23 01:08:06 +0100

    all

--- firestore_config.py
+++ firestore_config.py
@@ -1,20 +1,27 @@
 # firebase_config.py
 
 import os
+import json
 from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Load .env so FIREBASE_CREDENTIALS is available
+# Load .env locally (ignored by Render, but useful for dev)
 load_dotenv()
 
 db = None
 
 def initialize_firestore():
     global db
-    # only initialize once
     if not firebase_admin._apps:
-        cred_path = os.getenv("FIREBASE_CREDENTIALS")
-        cred = credentials.Certificate(cred_path)
-        firebase_admin.initialize_app(cred)
+        firebase_json = os.getenv("FIREBASE_CONFIG_JSON")
+        if not firebase_json:
+            raise ValueError("FIREBASE_CONFIG_JSON not found in environment variables.")
+        try:
+            cred_dict = json.loads(firebase_json)
+            cred = credentials.Certificate(cred_dict)
+            firebase_admin.initialize_app(cred)
+        except Exception as e:
+            print(f"Firebase initialization failed: {e}")
+            raise e
     db = firestore.client()
